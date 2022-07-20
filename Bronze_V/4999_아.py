977b1b30bcde848feed59c0fd70082ea51ca7457
lms20171466 <lys1978000@gmail.com>
2022-07-20 20:19:07 +0900

    '0720'

---
+++ Bronze_V/4999_아.py
@@ -0,0 +1,6 @@
+jahwan = input()
+doctor = input()
+if len(jahwan.split('h')[0]) >= len(doctor.split('h')[0]):
+    print('go')
+else:
+    print('no') 
---
+++ Bronze_V/7287_등록.py
@@ -0,0 +1,2 @@
+print(91)
+print('lys1978000')
